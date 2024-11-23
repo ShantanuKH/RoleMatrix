6516df4b0cf7a4285efbf685a44adcb7f773b1fb
ShantanuKH <khadseshantanu02@gmail.com>
2024-11-23 23:15:37 +0530

    RBAC

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  "./views/**/*.ejs",
+  "./public/**/*.html",
+  "./src/**/*.{js,jsx,ts,tsx}", // Include JavaScript/TypeScript files (if you're using React or similar)
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
